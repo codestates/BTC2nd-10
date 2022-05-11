f9c6545514c124696033f7347233552ae1e710f3
김동욱 <kdo6338@gmail.com>
2022-05-11 17:50:36 +0900

    모달창 제작

--- Avalanche/public/javascripts/index.js
+++ Avalanche/public/javascripts/index.js
@@ -1,11 +1,38 @@
 window.onload = () => {
-    document.getElementById('my_wallet').addEventListener('click',walletHandler)
+    document.getElementById('my_wallet').addEventListener('click',walletHandler);
 }
 
 const walletHandler = (e) => {
     let body = document.querySelector('body');
+    let box = document.createElement('div');
+    box.classList.add('modal')
     let modal = document.createElement('div');
-    modal.classList.add('modal');
-    modal.innerHTML = ``;
-    body.appendChild(modal);
+    modal.classList.add('modal_background');
+    let modal_content = document.createElement('div');
+    modal_content.classList.add('modal_content');
+
+    modal_content.innerHTML = `
+    <div class="modal_content_body">
+        <div>
+            <div>
+                <i class="fa-brands fa-get-pocket fa-5x"></i>
+            </div>
+            <label>개인키로 계정을 가져옵니다.</label>
+        </div>
+        <div>
+            <div>
+                <i class="fa-solid fa-plus fa-5x"></i>
+            </div>
+            <label>새로운 지갑을 생성합니다.</label>
+        </div>
+    </div>`
+
+    box.appendChild(modal_content);
+    box.appendChild(modal);
+
+    modal.addEventListener('click',function (){
+        box.remove();
+    })
+
+    body.appendChild(box);
 }
